ce9a4dc6a174d2d7bd479bde35af791cd668cbe7
Toon Wong <toonwwk@hotmail.com>
2020-02-25 00:05:42 +0700

    update pin

---
+++ AHKA_AR/View/MapCell.swift
@@ -0,0 +1,18 @@
+//
+//  MapCell.swift
+//  AHKA_AR
+//
+//  Created by Kanokporn Wongwaitayakul on 24/2/2563 BE.
+//  Copyright © 2563 boblancer. All rights reserved.
+//
+
+import UIKit
+
+class MapCell: UICollectionViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+}
