572dd3d7ed91475ae3cde53bec10e350515a9f72
Toon Wong <toonwwk@hotmail.com>
2020-02-24 20:19:05 +0700

    add asset, and map control

---
+++ AHKA_AR/Controller/mapController.swift
@@ -0,0 +1,15 @@
+//
+//  mapController.swift
+//  AHKA_AR
+//
+//  Created by Kanokporn Wongwaitayakul on 24/2/2563 BE.
+//  Copyright © 2563 boblancer. All rights reserved.
+//
+
+import UIKit
+class MapController: UIViewController{
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+}
