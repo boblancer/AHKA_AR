eea168e6259614f95e7a8250ceef08e719c515ed
Toon Wong <toonwwk@hotmail.com>
2020-02-26 01:10:49 +0700

    update pin reposition

--- AHKA_AR/Controller/mapController.swift
+++ AHKA_AR/Controller/mapController.swift
@@ -8,8 +8,69 @@
 
 import UIKit
 class MapController: UIViewController{
+    
+    @IBOutlet weak var mapView: UICollectionView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        mapView.dataSource = self
+        mapView.delegate = self
+        mapView.isPagingEnabled = true
+        mapView.register(UINib(nibName: "MapCell", bundle: nil), forCellWithReuseIdentifier: "MapCell")
+        mapView.reloadData()
     }
+}
+
+extension MapController: UICollectionViewDelegateFlowLayout, UICollectionViewDataSource{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 1
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell: MapCell = collectionView.dequeueReusableCell(withReuseIdentifier: "MapCell", for: indexPath) as! MapCell
+        
+        // 1795 1038
+        let img = #imageLiteral(resourceName: "map")
+        let heightRatio = mapView.frame.size.height / 1038
+        let width = 1795 * heightRatio
+        
+        let newImg = img.resizeImageWith(newSize: CGSize(width: width, height: mapView.frame.size.height))
+        
+        cell.mapView.image = newImg
+        cell.scrollView.contentSize = newImg.size
+        
+        for pin in cell.pinList{
+            let width = pin.frame.size.width * heightRatio
+            let height = pin.frame.size.height * heightRatio
+            let x = pin.frame.origin.x * heightRatio
+            let y = pin.frame.origin.y * heightRatio
+            
+            cell.pinPosition[pin] = [width, height, x, y]
+            pin.frame = CGRect(x: x , y: y, width: width, height: height)
+        
+        }
+        
+        for pin in cell.pinList2{
+            let width = pin.frame.size.width * heightRatio
+            let height = pin.frame.size.height * heightRatio
+            let x = pin.frame.origin.x * heightRatio
+            let y = pin.frame.origin.y * heightRatio
+            
+            cell.pinPosition[pin] = [width, height, x, y]
+            pin.frame = CGRect(x: x , y: y, width: width, height: height)
+        }
+        return cell
+    }
+
+    func collectionView (_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let yourWidth = mapView.bounds.width
+        let yourHeight = mapView.bounds.height
+        return CGSize(width: yourWidth, height: yourHeight)
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets.zero
+    }
+    
+    
 }
--- AHKA_AR/View/MapCell.swift
+++ AHKA_AR/View/MapCell.swift
@@ -8,11 +8,111 @@
 
 import UIKit
 
-class MapCell: UICollectionViewCell {
-
+class MapCell: UICollectionViewCell, UIScrollViewDelegate {
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet weak var mapView: UIImageView!
+    @IBOutlet weak var noName1: UIButton!
+    @IBOutlet weak var voodooHub: UIButton!
+    @IBOutlet weak var coffeeRoasting: UIButton!
+    @IBOutlet weak var noName2: UIButton!
+    @IBOutlet weak var chiefHub: UIButton!
+    @IBOutlet weak var visitorCenter: UIButton!
+    @IBOutlet weak var cuturalCenter: UIButton!
+    @IBOutlet weak var holyWell: UIButton!
+    @IBOutlet weak var ghostDoor: UIButton!
+    @IBOutlet weak var saoChingcha: UIButton!
+    @IBOutlet weak var coffee: UIButton!
+    @IBOutlet weak var skywalk: UIButton!
+    
+    @IBOutlet weak var text1: UIButton!
+    @IBOutlet weak var text2: UIButton!
+    @IBOutlet weak var text3: UIButton!
+    @IBOutlet weak var text4: UIButton!
+    
+    var pinList: [UIButton] = []
+    var pinList2: [UIButton] = []
+    var pinPosition: [UIButton: [CGFloat]] = [:]
+    
     override func awakeFromNib() {
         super.awakeFromNib()
-        // Initialization code
+        scrollView.delegate = self
+        scrollView.minimumZoomScale = 1.0
+        scrollView.maximumZoomScale = 2.0
+        setupPinPosition()
     }
+    
+    func setupPinPosition(){        
+        pinList = [noName1, voodooHub, noName2, chiefHub, cuturalCenter, holyWell, ghostDoor, saoChingcha, coffee, text1, text2]
+        pinList2 = [coffeeRoasting, visitorCenter, skywalk, text3, text4]
+    }
+    
+    func viewForZooming(in scrollView: UIScrollView) -> UIView? {
+        return self.scrollView.viewWithTag(5)
+    }
+    
+    func scrollViewDidZoom(_ scrollView: UIScrollView) {
+        //updatePinPosition()
+    }
+    
+    func updatePinPosition(){
+        //let heightRatio = scrollView.bounds.size.height / 1038
+        
+        for pin in pinList{
+            let newWidth = self.pinPosition[pin]![0] / scrollView.zoomScale
+            let newHeight = self.pinPosition[pin]![1] / scrollView.zoomScale
+            let difWidth = self.pinPosition[pin]![0] - newWidth
+            let difHeight = self.pinPosition[pin]![1] - newHeight
+            let newX = pinPosition[pin]![2] + (difWidth / 2)
+            let newY = pinPosition[pin]![3] + difHeight
+            
+            pin.frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+        }
+        
+        for pin in pinList2{
+            let newWidth = self.pinPosition[pin]![0] / scrollView.zoomScale
+            let newHeight = self.pinPosition[pin]![1] / scrollView.zoomScale
+            let difWidth = self.pinPosition[pin]![0] - newWidth
+            let difHeight = self.pinPosition[pin]![1] - newHeight
+            let newX = pinPosition[pin]![2]
+            let newY = pinPosition[pin]![3] 
+            
+            pin.frame = CGRect(x: newX, y: newY, width: newWidth, height: newHeight)
+        }
+        
+        
+    }
+    
+    
+    
+}
 
+
+
+extension MapCell{
+    
+    @IBAction func pinPressed(_ sender: UIButton) {
+        print(sender.accessibilityIdentifier)
+    }
+    
+}
+
+
+extension UIImage{
+    
+    func resizeImageWith(newSize: CGSize) -> UIImage {
+        
+        let horizontalRatio = newSize.width / size.width
+        let verticalRatio = newSize.height / size.height
+        
+        let ratio = max(horizontalRatio, verticalRatio)
+        let newSize = CGSize(width: size.width * ratio, height: size.height * ratio)
+        UIGraphicsBeginImageContextWithOptions(newSize, true, 0)
+        draw(in: CGRect(origin: CGPoint(x: 0, y: 0), size: newSize))
+        let newImage = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return newImage!
+    }
+    
+    
 }
