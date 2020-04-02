527b5c9519f83dc5397529ae311157a188091e38
Toon Wong <toonwwk@hotmail.com>
2020-04-02 21:28:57 +0700

    update ui + merge

--- AHKA_AR/Controller/ARViewController.swift
+++ AHKA_AR/Controller/ARViewController.swift
@@ -38,86 +38,88 @@ class ARViewController: UIViewController, ARSCNViewDelegate, UINavigationControl
         }
     }
     
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        
-        // Set the view's delegate
-        sceneView.delegate = self
-        
-        // Show statistics such as fps and timing information
-        sceneView.showsStatistics = true
-        setUpScene()
-        
-    }
-    
-    func setUpScene(){
-        let mainScene = SCNScene()
-        sceneView.scene = mainScene
-    }
-    
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        
-        // Create a session configuration
-        let configuration = ARWorldTrackingConfiguration()
-        if let detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "ARTarget",
-                                                                  bundle: Bundle.main){
-            configuration.detectionImages = detectionImages
-            configuration.maximumNumberOfTrackedImages = 1
+   override func viewDidLoad() {
+            super.viewDidLoad()
+            
+            // Set the view's delegate
+            sceneView.delegate = self
+            
+            // Show statistics such as fps and timing information
+            sceneView.showsStatistics = true
+            setUpScene()
+            
         }
-        // Run the view's session
-        sceneView.session.run(configuration)
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
         
-        // Pause the view's session
-        sceneView.session.pause()
-    }
-    
-    func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
-        let node = SCNNode()
-        let imageAnchor = (anchor is ARImageAnchor) ? anchor as? ARImageAnchor : nil
-        if (imageAnchor != nil) && !(self.persistetService.getBooleanValueForKey(key: imageAnchor!.referenceImage.name!)) {
-            DispatchQueue.global().async {
-                //            let plane = SCNPlane(width: 10, height: 10)
-                //            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
-                //            let planeNode = SCNNode(geometry: plane
-                let s = (imageAnchor!.referenceImage.name)!
-                let characterIndex = s[(s.index(s.startIndex, offsetBy: 10)..<s.endIndex)]
-                print("the index is ", characterIndex, ".scn")
-                let characterScn = SCNScene(named: "art.scnassets/\(characterIndex).scn")
-                let characterNode = characterScn?.rootNode
-                let c = (characterIndex as NSString).integerValue
-                characterNode?.scale = SCNVector3(c, c, c)
-                node.addChildNode(characterNode!)
-                self.persistetService.saveBoolean(key: String(characterIndex), value: true)
-                self.player.playSound(resourceName: String(characterIndex))
+        func setUpScene(){
+            let mainScene = SCNScene()
+            sceneView.scene = mainScene
+        }
+        
+        override func viewWillAppear(_ animated: Bool) {
+            super.viewWillAppear(animated)
+            
+            // Create a session configuration
+            let configuration = ARWorldTrackingConfiguration()
+            if let detectionImages = ARReferenceImage.referenceImages(inGroupNamed: "ARTarget",
+                                                                      bundle: Bundle.main){
+                configuration.detectionImages = detectionImages
+                configuration.maximumNumberOfTrackedImages = 1
             }
+            // Run the view's session
+            sceneView.session.run(configuration)
         }
-        return node
-    }
-    
-    // MARK: - ARSCNViewDelegate
-    
-    func session(_ session: ARSession, didFailWithError error: Error) {
-        // Present an error message to the user
+        
+        override func viewWillDisappear(_ animated: Bool) {
+            super.viewWillDisappear(animated)
+            
+            // Pause the view's session
+            sceneView.session.pause()
+        }
+        
+        func renderer(_ renderer: SCNSceneRenderer, nodeFor anchor: ARAnchor) -> SCNNode? {
+            let node = SCNNode()
+            let imageAnchor = (anchor is ARImageAnchor) ? anchor as? ARImageAnchor : nil
+            if (imageAnchor != nil) && !(self.persistetService.getBooleanValueForKey(key: imageAnchor!.referenceImage.name!)) {
+                DispatchQueue.global().async {
+                    //            let plane = SCNPlane(width: 10, height: 10)
+                    //            plane.firstMaterial?.diffuse.contents = UIColor.white.withAlphaComponent(0.5)
+                    //            let planeNode = SCNNode(geometry: plane
+                    let s = (imageAnchor!.referenceImage.name)!
+                    let characterIndex = s[(s.index(s.startIndex, offsetBy: 10)..<s.endIndex)]
+                    print("the index is ", characterIndex, ".scn")
+                    let characterScn = SCNScene(named: "art.scnassets/\(characterIndex).scn")
+                    let characterNode = characterScn?.rootNode
+                    let c = self.scale[Int(String(characterIndex))! - 1]
+                    print("scale is ", c)
+                    characterNode?.scale = SCNVector3(c, c, c)
+                    characterNode?.eulerAngles.x = -.pi/2
+                    node.addChildNode(characterNode!)
+                    self.persistetService.saveBoolean(key: String(characterIndex), value: true)
+                    self.player.playSound(resourceName: String(characterIndex))
+                }
+            }
+            return node
+        }
+        
+        // MARK: - ARSCNViewDelegate
+        
+        func session(_ session: ARSession, didFailWithError error: Error) {
+            // Present an error message to the user
+            
+        }
+        
+        func sessionWasInterrupted(_ session: ARSession) {
+            // Inform the user that the session has been interrupted, for example, by presenting an overlay
+        }
+        
+        func sessionInterruptionEnded(_ session: ARSession) {
+            // Reset tracking and/or remove existing anchors if consistent tracking is required
+            
+        }
+        
+        @IBAction func homeButtonPressed(_ sender: UIButton) {
+            self.dismiss(animated: true, completion: nil)
+        }
+        
         
     }
-    
-    func sessionWasInterrupted(_ session: ARSession) {
-        // Inform the user that the session has been interrupted, for example, by presenting an overlay
-    }
-    
-    func sessionInterruptionEnded(_ session: ARSession) {
-        // Reset tracking and/or remove existing anchors if consistent tracking is required
-        
-    }
-    
-    @IBAction func homeButtonPressed(_ sender: UIButton) {
-        self.dismiss(animated: true, completion: nil)
-    }
-    
-    
-}
--- AHKA_AR/Controller/MapController.swift
+++ AHKA_AR/Controller/MapController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import AVFoundation
 class MapController: UIViewController, PinDelegate{
     @IBOutlet weak var bottomBar: UIView!
     @IBOutlet weak var mapView: UICollectionView!
@@ -14,43 +15,74 @@ class MapController: UIViewController, PinDelegate{
     @IBOutlet weak var slideView: UIScrollView!
     @IBOutlet weak var pageControl: UIPageControl!
     @IBOutlet weak var popup: UIView!
+    @IBOutlet weak var slideViewLeading: NSLayoutConstraint!
     
     let infoSlide:HowToSlide = Bundle.main.loadNibNamed("HowToSlide", owner: self, options: nil)?.first as! HowToSlide
     let pinKeyList = ["01" ,"02" ,"03" ,"04" ,"05" ,"06" ,"07" ,"08" ,"09" ,"10" ,"11" ,"12"]
-
+    
     let foundPinList = ["holyWell" ,"skywalk" ,"noName2" ,"coffee" ,"voodooHub","cuturalCenter" ,"noName1" ,"visitorCenter" ,"chiefHub","saoChingcha" ,"coffeeRoasting" ,"ghostDoor"]
     let defaults = UserDefaults.standard
+    
+    var timer: Timer? = nil
 
+    var player: AVAudioPlayer?
     var howToSlides: [HowToSlide] = []
     var map = MapCell()
     
     override func viewDidLoad() {
         super.viewDidLoad()
+//        defaults.set(true, forKey: "01")
+//        defaults.set(true, forKey: "02")
+//        defaults.set(true, forKey: "03")
+//        defaults.set(true, forKey: "04")
+//        defaults.set(true, forKey: "05")
+//        defaults.set(true, forKey: "06")
+//        defaults.set(true, forKey: "07")
+//        defaults.set(true, forKey: "08")
+//        defaults.set(true, forKey: "09")
+//        defaults.set(true, forKey: "10")
+//        defaults.set(true, forKey: "11")
+//        defaults.set(true, forKey: "12")
+        playSound()
         popup.isHidden = false
         slideView.delegate = self
-        infoSlide.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: view.frame.height - 95)
-
+        
         mapView.dataSource = self
         mapView.delegate = self
         mapView.isPagingEnabled = true
         mapView.register(UINib(nibName: "MapCell", bundle: nil), forCellWithReuseIdentifier: "MapCell")
         mapView.reloadData()
         
-        
+
         UIView.animate(withDuration: 0, delay: 0, usingSpringWithDamping: 0, initialSpringVelocity: 0, options: .curveEaseOut, animations: {
             self.popup.transform = CGAffineTransform(translationX: 0, y: self.popup.frame.height)
         })
-        
+        print(defaults.bool(forKey: "Congrats"))
+        if timer == nil && defaults.bool(forKey: "Congrats") != true{
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(bouncePins), userInfo: nil, repeats: true)
+        }
     }
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
         
+        
+        if player == nil{
+            playSound()
+        }
+        else{
+            player?.play()
+        }
+        
+        if timer == nil && defaults.bool(forKey: "Congrats") != true{
+            timer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(bouncePins), userInfo: nil, repeats: true)
+        }
+        
         var check = true
         if map.pinList.count == 12{
             for index in 0...11{
                 if defaults.bool(forKey: pinKeyList[index]) == true{
-                    let imageTitle = foundPinList[index]
+                    let imageTitle = foundPinList[index] + "Found"
                     if let image = UIImage(named: imageTitle) {
                         map.pinList[index].setImage(image, for: .normal)
                         map.pinList[index].accessibilityIdentifier = imageTitle
@@ -73,26 +105,87 @@ class MapController: UIViewController, PinDelegate{
                 }
                 
                 slideView.contentSize = CGSize(width: view.frame.width, height: slideView.frame.height)
-
-
+                
+                
                 infoSlide.images.image = UIImage(named: "congrat")
                 
                 slideView.addSubview(infoSlide)
                 UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-                           self.popup.transform = CGAffineTransform(translationX: 0, y: 0)
-                           self.bottomBar.transform = CGAffineTransform(translationX: 0, y: self.bottomBar.frame.height)
+                    self.popup.transform = CGAffineTransform(translationX: 0, y: 0)
+                    self.bottomBar.transform = CGAffineTransform(translationX: 0, y: self.bottomBar.frame.height)
                 })
             }
         }
     }
     
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+
+        if player != nil{
+            player?.pause()
+        }
+        
+        if timer != nil {
+            timer?.invalidate()
+            timer = nil
+        }
+    }
+  
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
+        // 3009 4892
+        
+        
+        infoSlide.frame = CGRect(x: 0, y: 0, width: view.frame.width, height: slideView.frame.height)
+        
         createSlides()
+        
+        let xRatio: CGFloat = 13 / 167
+        let xWidth = view.frame.width / 5
+        let xShadow = xRatio * xWidth
+        let xX = view.frame.midX - (xWidth / 2) + (xShadow)
+        let screenWidth = view.frame.width
+        let screenRatio = screenWidth / 3009
+        let bottomSpace = 205 * screenRatio
+        var contentHeight = 4892 * screenRatio
+        
+        if contentHeight > slideView.frame.height{
+            contentHeight = slideView.frame.height
+            let leftY = (view.frame.height - (contentHeight + 25)) / 2
+            let closeButtonHeight = (contentHeight + 25) + leftY - (xWidth / 2) - (bottomSpace / 2)
+            
+            pageControl.frame = CGRect(x: view.frame.midX - (pageControl.frame.width / 2), y: contentHeight, width: pageControl.frame.width, height: pageControl.frame.height)
+            closeButton.frame = CGRect(x:  xX, y: closeButtonHeight + 6, width: xWidth, height: xWidth)
+        }
+        else{
+            let leftY = (view.frame.height - (contentHeight + 25)) / 2
+            let closeButtonHeight = (contentHeight + 25) + leftY - (xWidth / 2) - (bottomSpace / 2)
+            
+            pageControl.frame = CGRect(x: view.frame.midX - (pageControl.frame.width / 2), y: contentHeight + 25, width: pageControl.frame.width, height: pageControl.frame.height)
+            closeButton.frame = CGRect(x:  xX, y: closeButtonHeight + 25, width: xWidth, height: xWidth)
+        }
+        
     }
+    
+    func playSound() {
+       
+        let url = Bundle.main.url(forResource: "bgsound", withExtension: "mp3")
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+            player = try AVAudioPlayer(contentsOf: url!, fileTypeHint: AVFileType.mp3.rawValue)
+            guard let player = player else { return }
+            player.numberOfLoops = -1
+            player.play()
 
+        } catch let error {
+            print(error.localizedDescription)
+        }
+
+    }
+    
     func createSlides(){
-        let images: [UIImage] = [#imageLiteral(resourceName: "howTo3"),#imageLiteral(resourceName: "howTo1"), #imageLiteral(resourceName: "howTo2"), #imageLiteral(resourceName: "howTo4")]
+        let images: [UIImage?] = [UIImage(named: "howTo1"), UIImage(named: "howTo2"), UIImage(named: "howTo3"), UIImage(named: "howTo4"), UIImage(named: "howTo5")]
         
         for i in 0..<images.count{
             let howTo:HowToSlide = Bundle.main.loadNibNamed("HowToSlide", owner: self, options: nil)?.first as! HowToSlide
@@ -100,6 +193,8 @@ class MapController: UIViewController, PinDelegate{
             howTo.frame = CGRect(x: view.frame.width * CGFloat(i), y: 0, width: view.frame.width, height: slideView.frame.height)
             howToSlides.append(howTo)
         }
+        pageControl.numberOfPages = 5
+
     }
     
     func clearPopup(){
@@ -114,33 +209,32 @@ class MapController: UIViewController, PinDelegate{
         for pin in self.map.pinList{
             pin.alpha = 1
         }
-
+        
         
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             self.popup.transform = CGAffineTransform(translationX: 0, y: self.popup.frame.height)
             self.bottomBar.transform = CGAffineTransform(translationX: 0, y: 0)
-
+            
         })
     }
     
     @IBAction func howToPlayPressed(_ sender: UIButton) {
+        
         popup.isHidden = false
-
         clearPopup()
-        pageControl.numberOfPages = 4
         pageControl.isHidden = false
         map.mapView.alpha = 0.5
         
         for pin in map.pinList{
             pin.alpha = 0.7
         }
-
-        slideView.contentSize = CGSize(width: view.frame.width * 4, height: slideView.frame.height)
+        
+        slideView.contentSize = CGSize(width: view.frame.width * 5, height: slideView.frame.height)
         for howto in howToSlides{
             slideView.addSubview(howto)
         }
-
-
+        
+        
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
             self.popup.transform = CGAffineTransform(translationX: 0, y: 0)
             self.bottomBar.transform = CGAffineTransform(translationX: 0, y: self.bottomBar.frame.height)
@@ -149,6 +243,7 @@ class MapController: UIViewController, PinDelegate{
     }
     
     func pinIsPressed(_ mapCell: MapCell, _ imageTitle : String){
+        
         pageControl.isHidden = true
         popup.isHidden = false
         clearPopup()
@@ -159,14 +254,15 @@ class MapController: UIViewController, PinDelegate{
         }
         
         slideView.contentSize = CGSize(width: view.frame.width, height: slideView.frame.height)
+        
         if let image = UIImage(named: imageTitle){
             infoSlide.images.image = image
         }
         
         slideView.addSubview(infoSlide)
         UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseOut, animations: {
-                   self.popup.transform = CGAffineTransform(translationX: 0, y: 0)
-                   self.bottomBar.transform = CGAffineTransform(translationX: 0, y: self.bottomBar.frame.height)
+            self.popup.transform = CGAffineTransform(translationX: 0, y: 0)
+            self.bottomBar.transform = CGAffineTransform(translationX: 0, y: self.bottomBar.frame.height)
         })
     }
     
@@ -211,14 +307,16 @@ extension MapController: UICollectionViewDelegateFlowLayout, UICollectionViewDat
         cell.mapView.image = newImg
         cell.scrollView.contentSize = newImg.size
         
-        for pin in cell.pinList{
+        for index in 0...11{
+            let pin = cell.pinList[index]
+            let realPin = cell.realPinList[index]
             let width = pin.frame.size.width * heightRatio
             let height = pin.frame.size.height * heightRatio
             let x = pin.frame.origin.x * heightRatio
             let y = pin.frame.origin.y * heightRatio
             
             pin.frame = CGRect(x: x , y: y, width: width, height: height)
-            
+            realPin.frame = CGRect(x: x , y: y, width: width, height: height)
         }
         
         for text in cell.textList{
@@ -255,6 +353,25 @@ extension MapController: UICollectionViewDelegateFlowLayout, UICollectionViewDat
         return UIEdgeInsets.zero
     }
     
+    @objc func bouncePins(){
+        UIView.animate(withDuration: 1, delay: 0, usingSpringWithDamping: 0.5, initialSpringVelocity: 0.5, options: .curveEaseIn, animations: {
+            for pin in self.map.pinList{
+                if pin.accessibilityIdentifier!.contains("Found") == false {
+                    pin.transform = CGAffineTransform(scaleX: 1.1, y: 1.1)
+
+                }
+            }
+        }) { (_) in
+            UIView.animate(withDuration: 1) {
+                for pin in self.map.pinList{
+                    if pin.accessibilityIdentifier!.contains("Found") == false{
+                        pin.transform = CGAffineTransform.identity
+                    }
+                }
+            }
+        }
+        
+    }
     
 }
 
